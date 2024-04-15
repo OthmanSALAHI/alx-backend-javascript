226fc4429d80e04d2f7740784687606f7704f52c
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-15 21:18:08 +0100

    Update student grades by city in 0x03-ES6_data_manipulation/4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrade) {
   return students.filter(({ location }) => location === city)
     .map((student) => {
-      const studentGrade = newGrade.filter(({ id }) => student.id === id);
-      let grade = 'N/A';
-      if (studentGrade.length > 0) {
-        grade = studentGrade[0] || 'N/A';
-      }
+      const studentGrade = newGrade.filter(({ studentId }) => student.id === studentId);
+      const { grade = 'N/A' } = studentGrade.length > 0 ? studentGrade[0] : {};
       return { ...student, grade };
     });
 }
