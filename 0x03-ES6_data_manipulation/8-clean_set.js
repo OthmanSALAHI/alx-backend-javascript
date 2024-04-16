7b29fcf1626dcfbaf216f2f70c6204678f35727a
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-16 11:47:14 +0100

    Fix cleanSet function to handle invalid input and improve string validation in 0x03-ES6_data_manipulation/8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-    if ( !(set instanceof Set) || !set || typeof startString !== 'string') return '';
+  if (!(set instanceof Set) || !set || typeof startString !== 'string') return '';
   let result = '';
   for (const val of set) {
-    if (val.startsWith(startString)) {
+    if (val.startsWith(startString) && typeof val === 'string') {
       result += `${val.slice(startString.length)}-`;
     }
   }
   if (!set || !startString) return '';
-  return result.slice(0, -1) 
+  return result.slice(0, -1);
 }
